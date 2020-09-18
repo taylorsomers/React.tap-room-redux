6c599fe58d3a41ec7102854e1beb05d394dbe68f
Taylor Somers <tsomers@yandex.com>
2020-09-18 14:22:24 -0400

    Import Redux tools and set up store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+const store = createStore(rootReducer);
+
+store.subscribe(() =>
+  store.getState()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
