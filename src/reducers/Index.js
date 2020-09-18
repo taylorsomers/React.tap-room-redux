571d14121afa5e5ea035bb2ff910744ed5e92194
Taylor Somers <tsomers@yandex.com>
2020-09-18 16:37:52 -0400

    Create rootReducer and combine form-visible-reducer and keg-list-reducer

---
+++ src/reducers/Index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import formVisibleReducer from './form-visible-reducer';
+import kegListReducer from './keg-list-reducer';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterKegList: kegListReducer
+});
+
+export default rootReducer;
