aa22de8738d0d20ce724746307e33e6ef4c0cfcb
Taylor Somers <tsomers@yandex.com>
2020-09-18 15:01:47 -0400

    Add logic to pass test for adding keg to masterKegList

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,21 +1,20 @@
 export default (state = {}, action) => {
-  return state;
-  // const { kegName, brewery, price, alcoholContent, pints, id } = action;
-  // switch (action.type) {
-  //   case 'ADD_KEG':
-  //     return Object.assign({}, state, {
-  //       [id]: {
-  //         kegName: kegName,
-  //         brewery: brewery,
-  //         price: price,
-  //         alcoholContent: alcoholContent,
-  //         pints: pints,
-  //         id: id
-  //       }
-  //     });
+  const { kegName, brewery, price, alcoholContent, pints, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          kegName: kegName,
+          brewery: brewery,
+          price: price,
+          alcoholContent: alcoholContent,
+          pints: pints,
+          id: id
+        }
+      });
   //   case 'DELETE_KEG':
   //     return 
-  //   default:
-  //     return state;
-  // }
+    default:
+      return state;
+  }
 }
