73af991ba9071cf1c4b3570713695d83ba0bc4f9
Taylor Somers <tsomers@yandex.com>
2020-09-18 19:44:55 -0400

    Add failing test for 'ADD_KEG' action creator

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,9 +1,21 @@
 import * as actions from './../../actions';
 
 describe('tap room actions', () => {
-  it('deleteKeg should create DELETE_KEG action', () => {
-    expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_KEG',
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({
+      kegName: 'Weasel Whistle',
+      brewery: 'Old Weasel',
+      price: '$45',
+      alcoholContent: 15,
+      pints: 124,
+      id: 1
+    })).toEqual({
+      type: 'ADD_KEG',
+      kegName: 'Weasel Whistle',
+      brewery: 'Old Weasel',
+      price: '$45',
+      alcoholContent: 15,
+      pints: 124,
       id: 1
     });
   });
@@ -13,4 +25,12 @@ describe('tap room actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
+  
+  it('deleteKeg should create DELETE_KEG action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: 'DELETE_KEG',
+      id: 1
+    });
+  });
+
 });
