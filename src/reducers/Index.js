75bd2e98f1ae91f987674154db36f9f1d6788ff6
Taylor Somers <tsomers@yandex.com>
2020-09-18 17:53:56 -0400

    Re-add code for rootReducer

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,10 +1,10 @@
-// import { combineReducers } from 'redux';
-// import formVisibleReducer from './form-visible-reducer';
-// import kegListReducer from './keg-list-reducer';
+import { combineReducers } from 'redux';
+import formVisibleReducer from './form-visible-reducer';
+import kegListReducer from './keg-list-reducer';
 
-// const rootReducer = combineReducers({
-//   formVisibleOnPage: formVisibleReducer,
-//   masterKegList: kegListReducer
-// });
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterKegList: kegListReducer
+});
 
-// export default rootReducer;
+export default rootReducer;
