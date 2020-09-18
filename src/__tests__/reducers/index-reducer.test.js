2db1820168366998ab5c46b0940f20de2cba018b
Taylor Somers <tsomers@yandex.com>
2020-09-18 18:03:13 -0400

    Import createStore from Redux and create Redux store for testing

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,7 @@
+import { createStore } from 'redux';
 import rootReducer from '../../reducers/index';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test('Should return default state if no action type is recognized', () => {
