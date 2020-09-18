e26e585a937493f3203dfb01aa9f436412b33348
Taylor Somers <tsomers@yandex.com>
2020-09-18 14:53:06 -0400

    Add undefined action and sample kegData for testing

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,17 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    kegName: 'Weasel Whistle',
+    brewery: 'Old Weasel Brewery',
+    price: '$45',
+    alcoholContent: 15,
+    pints: 124,
+    id: 1
+  };
+
   test('Should return default state i f there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
