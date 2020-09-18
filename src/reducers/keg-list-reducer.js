5c72fcfff784fc72e9e20d7f1c1b072f979cb787
Taylor Somers <tsomers@yandex.com>
2020-09-18 14:44:26 -0400

    Write failing test for kegListReducer

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,18 +1,20 @@
 export default (state = {}, action) => {
-  const { kegName, brewery, price, alcoholContent, pints, id } = action;
-  switch (action.type) {
-    case 'ADD_KEG':
-      return Object.assign({}, state, {
-        [id]: {
-          kegName: kegName,
-          brewery: brewery,
-          price: price,
-          alcoholContent: alcoholContent,
-          pints: pints,
-          id: id
-        }
-      });
-    default:
-      return state;
-  }
+  // const { kegName, brewery, price, alcoholContent, pints, id } = action;
+  // switch (action.type) {
+  //   case 'ADD_KEG':
+  //     return Object.assign({}, state, {
+  //       [id]: {
+  //         kegName: kegName,
+  //         brewery: brewery,
+  //         price: price,
+  //         alcoholContent: alcoholContent,
+  //         pints: pints,
+  //         id: id
+  //       }
+  //     });
+  //   case 'DELETE_KEG':
+  //     return 
+  //   default:
+  //     return state;
+  // }
 }
