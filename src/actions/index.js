bd8007ed71dfafd8b10374ee11cc789f86218d39
Taylor Somers <tsomers@yandex.com>
2020-09-18 19:48:05 -0400

    Add test-passing 'ADD_KEG' action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
+export const addKeg = (keg) => {
+  const { kegName, brewery, price, alcoholContent, pints, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    kegName: kegName,
+    brewery: brewery,
+    price: price,
+    alcoholContent: alcoholContent,
+    pints: pints,
+    id: id
+  }
+}
+
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
